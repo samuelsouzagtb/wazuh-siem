eab5afa886f7ec6a9283654cd8443697043f97af
Samuel Souza <samukasouza86@outlook.com>
2025-05-07 15:48:38 -0300

    Ajustada geração certificados

--- k8s/generate_certs.sh
+++ k8s/generate_certs.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Create directory for certificates
-mkdir -p k8s/certs
+# Create directories for certificates
+mkdir -p k8s/certs/indexer
+mkdir -p k8s/certs/manager
+mkdir -p k8s/certs/dashboard
 
-# Generate private key and certificate
+# Generate private key and certificate for Wazuh Indexer
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout k8s/certs/dashboard-key.pem \
-  -out k8s/certs/dashboard-cert.pem \
+  -keyout k8s/certs/indexer/indexer-key.pem \
+  -out k8s/certs/indexer/indexer-cert.pem \
+  -subj "/CN=wazuh-indexer"
+
+# Generate private key and certificate for Wazuh Manager
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout k8s/certs/manager/manager-key.pem \
+  -out k8s/certs/manager/manager-cert.pem \
+  -subj "/CN=wazuh-manager"
+
+# Generate private key and certificate for Wazuh Dashboard
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout k8s/certs/dashboard/dashboard-key.pem \
+  -out k8s/certs/dashboard/dashboard-cert.pem \
   -subj "/CN=wazuh-dashboard"
